3a504b4c9fa983e309edd1f08178f0d2fdf15e25
eriksun0310 <113155520+eriksun0310@users.noreply.github.com>
2024-09-06 14:41:31 +0800

    216 end

--- shopping_list_app/lib/widget/new_item.dart
+++ shopping_list_app/lib/widget/new_item.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:shopping_list_app/data/categories.dart';
+import 'package:shopping_list_app/models/category.dart';
 
 class NewItem extends StatefulWidget {
   const NewItem({super.key});
@@ -12,9 +13,18 @@ class NewItem extends StatefulWidget {
 
 class _NewItemState extends State<NewItem> {
   final _formKey = GlobalKey<FormState>();
+  var _enteredName = '';
+  var _enteredQuantity = 1;
+  var _selectedCategory = categories[Categories.vegetables]!;
 
   void _saveItem() {
-    _formKey.currentState!.validate();
+    // 驗證成功,才可以儲存
+    if (_formKey.currentState!.validate()) {
+      _formKey.currentState!.save();
+      print(_enteredName);
+      print(_enteredQuantity);
+      print(_selectedCategory);
+    }
   }
 
   @override
@@ -41,6 +51,9 @@ class _NewItemState extends State<NewItem> {
                   }
                   return null;
                 },
+                onSaved: (value) {
+                  _enteredName = value!;
+                },
               ),
               Row(
                 crossAxisAlignment: CrossAxisAlignment.end,
@@ -53,36 +66,49 @@ class _NewItemState extends State<NewItem> {
                       if (value == null ||
                           value.isEmpty ||
                           // 輸入不為數字
+                          //tryParse: 字串轉數字(如果轉換失敗,則會回傳null)
                           int.tryParse(value) == null ||
                           int.tryParse(value)! <= 0) {
                         return 'Must be a valid positive number.  ';
                       }
                       return null;
                     },
-                    initialValue: '1',
+                    initialValue: _enteredQuantity.toString(),
+                    onSaved: (value) {
+                      //parse: 字串轉數字(如果轉換失敗,則會回傳錯誤)
+                      _enteredQuantity = int.parse(value!);
+                    },
                   )),
                   const SizedBox(
                     width: 8,
                   ),
                   Expanded(
-                      child: DropdownButtonFormField(items: [
-                    for (final category in categories.entries)
-                      DropdownMenuItem(
-                          value: category.value,
-                          child: Row(
-                            children: [
-                              Container(
-                                width: 16,
-                                height: 16,
-                                color: category.value.color,
-                              ),
-                              const SizedBox(
-                                width: 6,
-                              ),
-                              Text(category.value.title)
-                            ],
-                          ))
-                  ], onChanged: (value) {}))
+                      child: DropdownButtonFormField(
+                          value: _selectedCategory, //初始值
+                          items: [
+                            for (final category in categories.entries)
+                              DropdownMenuItem(
+                                  value: category.value,
+                                  child: Row(
+                                    children: [
+                                      Container(
+                                        width: 16,
+                                        height: 16,
+                                        color: category.value.color,
+                                      ),
+                                      const SizedBox(
+                                        width: 6,
+                                      ),
+                                      Text(category.value.title)
+                                    ],
+                                  ))
+                          ],
+                          onChanged: (value) {
+                            // 要設置狀態是因為 下拉列表中選擇的內容畫面保持同步
+                            setState(() {
+                              _selectedCategory = value!;
+                            });
+                          }))
                 ],
               ),
               const SizedBox(height: 12),
